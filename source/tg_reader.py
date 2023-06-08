4da1b1ecfe11d5a0b27067fb3c4ce1e3af5aa0d6
BuildTools <unconfigured@null.spigotmc.org>
2023-06-08 16:35:04 -0500

    adding bam support to tg_reader

--- source/tg_reader.py
+++ source/tg_reader.py
@@ -1,34 +1,44 @@
+import pysam
 import gzip
 import sys
 
 #
-# accepts fq / fq.gz / fa / fa.gz
+# accepts fq / fq.gz / fa / fa.gz / bam
 #
 # handles fa files with newlines in read sequence
 #
+
 
 class TG_Reader:
     def __init__(self, input_filename, verbose=True):
         self.verbose = verbose
         fnl = input_filename.lower()
         #
-        if fnl[-2:].lower() == 'fq' or fnl[-5:].lower() == 'fq.gz' or fnl[-5:].lower() == 'fastq' or fnl[-8:].lower() == 'fastq.gz':
+        if fnl[-2:] == 'fq' or fnl[-5:] == 'fq.gz' or fnl[-5:] == 'fastq' or fnl[-8:] == 'fastq.gz':
             self.filetype = 'FASTQ'
-        elif fnl[-2:].lower() == 'fa' or fnl[-5:].lower() == 'fa.gz' or fnl[-5:].lower() == 'fasta' or fnl[-8:].lower() == 'fasta.gz':
+        elif fnl[-2:] == 'fa' or fnl[-5:] == 'fa.gz' or fnl[-5:] == 'fasta' or fnl[-8:] == 'fasta.gz':
             self.filetype = 'FASTA'
+        elif fnl[-3:] == 'bam':
+            self.filetype = 'BAM'
         else:
             print('Error: unknown file suffix given to TG_Reader:')
             print(input_filename)
             exit(1)
         #
-        if fnl[-3:] == '.gz':
-            if self.verbose:
-                print('getting reads from gzipped ' + self.filetype + '...')
-            self.f = gzip.open(input_filename, 'rt')
-        else:
+        if self.filetype == 'BAM':
             if self.verbose:
                 print('getting reads from ' + self.filetype + '...')
-            self.f = open(input_filename, 'r')
+            self.f = pysam.AlignmentFile(input_filename, "rb", ignore_truncation=True, check_sq=False)
+            self.alns = self.f.fetch(until_eof=True)
+        else:
+            if fnl[-3:] == '.gz':
+                if self.verbose:
+                    print('getting reads from gzipped ' + self.filetype + '...')
+                self.f = gzip.open(input_filename, 'rt')
+            else:
+                if self.verbose:
+                    print('getting reads from ' + self.filetype + '...')
+                self.f = open(input_filename, 'r')
         #
         self.buffer = []
         self.current_readname = None
@@ -69,6 +79,22 @@ class TG_Reader:
                 self.current_readname = self.buffer[-1][1:]
                 self.buffer = []
             return out_dat
+        #
+        elif self.filetype == 'BAM':
+            try:
+                aln = next(self.alns)
+                my_name = aln.qname
+                try:
+                    my_np = aln.get_tag('np')
+                    my_rq = aln.get_tag('rq')
+                    my_name += ' np={0} rq={1:0.6f}'.format(my_np, my_rq)
+                # np or rq tag is not present
+                except KeyError:
+                    pass
+                return (my_name, aln.query, aln.qual)
+            # this can happen if file is truncated
+            except OSError:
+                return ('','','')
 
     #
     # returns list of [(readname1, readsequence1, qualitysequence1), (readname2, readsequence2, qualitysequence2), ...]
@@ -97,6 +123,8 @@ def quick_grab_all_reads(fn):
 #
 # a quick test
 #
+
+
 if __name__ == '__main__':
     #
     IN_READS_TEST = sys.argv[1]
