c1f087fa482508432ab65442408501a2e3d80969
zstephens <unconfigured@null.spigotmc.org>
2023-11-15 18:28:50 -0600

    it's corn time

--- source/tg_ref.py
+++ source/tg_ref.py
@@ -8,15 +8,24 @@ class ReferenceFasta:
         self.ref_inds = self.index_ref()
         self.ri_rev   = {self.ref_inds[n][0]:n for n in range(len(self.ref_inds))}
 
+    def get_contig_names(self):
+        return [n[0] for n in self.ref_inds]
+
     def get_refseq(self, contig_name):
         if contig_name not in self.ri_rev:
             print('Warning: contig not found in reference file:', contig_name)
             return None
         my_ri = self.ri_rev[contig_name]
-        ref_file = open(self.ref_fn,'r')
-        ref_file.seek(self.ref_inds[my_ri][1])
-        my_dat = ''.join(ref_file.read(int(self.ref_inds[my_ri][2]) - int(self.ref_inds[my_ri][1])).split('\n'))
-        ref_file.close()
+        if self.ref_fn[-3:] == '.gz':
+            # reading entire reference into memory, this is suboptimal but indexing gzipped files seems complicated
+            with gzip.open(self.ref_fn, 'rt') as ref_file:
+                fr = ref_file.read()
+            my_dat = fr[int(self.ref_inds[my_ri][1]):int(self.ref_inds[my_ri][2])].replace('\n','')
+        else:
+            with open(self.ref_fn,'r') as ref_file:
+                ref_file = open(self.ref_fn,'r')
+                ref_file.seek(self.ref_inds[my_ri][1])
+            my_dat = ref_file.read(int(self.ref_inds[my_ri][2]) - int(self.ref_inds[my_ri][1])).replace('\n','')
         return my_dat
 
     def index_ref(self, verbose=False):
@@ -30,26 +39,31 @@ class ReferenceFasta:
                 print('found fasta index ' + self.ref_fn + '.fai')
             fn = self.ref_fn + '.fai'
         #
+        if self.ref_fn[-3:] == '.gz':
+            fn = None
+        #
         ref_inds = []
         if fn is not None:
-            fai = open(fn,'r')
-            for line in fai:
-                splt = line[:-1].split('\t')
-                seqlen  = int(splt[1])
-                offset  = int(splt[2])
-                linelen = int(splt[3])
-                nlines  = seqlen // linelen
-                if seqlen % linelen != 0:
-                    nlines += 1
-                ref_inds.append((splt[0], offset, offset + seqlen + nlines, seqlen))
-            fai.close()
+            with open(fn,'r') as fai:
+                for line in fai:
+                    splt = line[:-1].split('\t')
+                    seqlen  = int(splt[1])
+                    offset  = int(splt[2])
+                    linelen = int(splt[3])
+                    nlines  = seqlen // linelen
+                    if seqlen % linelen != 0:
+                        nlines += 1
+                    ref_inds.append((splt[0], offset, offset + seqlen + nlines, seqlen))
         else:
             if verbose:
                 print('fasta index not found, creating one...')
             prevR = None
             prevP = None
             seqlen = 0
-            f = open(self.ref_fn, 'r')
+            if self.ref_fn[-3:] == '.gz':
+                f = gzip.open(self.ref_fn, 'rt')
+            else:
+                f = open(self.ref_fn, 'r')
             while True:
                 data = f.readline()
                 if not data:
